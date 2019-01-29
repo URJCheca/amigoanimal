6d52e91b1569f65bc77832100c91c425955e5199
alexi <alexi@10.0.6.201>
2019-01-29 13:29:43 +0100

    Creacion de Producto

---
+++ src/main/java/com/dad/amigoanimal/Producto.java
@@ -0,0 +1,41 @@
+package com.dad.amigoanimal;
+
+public class Producto {
+
+	private String name;
+	private float price;
+	//private String photo;
+	private String category;
+	
+	public Producto(String name, float price, String category) {
+		this.name = name;
+		this.price = price;
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+}
